c9dbbad1214af4d2c55196368dc95c09e6f559d9
n1shez <matvey.rudenko.2008@gmail.com>
2023-10-22 15:35:56 +0300

    arr random

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <math.h>
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	const int SIZE = 10;
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 20;
+		for (int k = 0; k < i; k++)
+		{
+			if (arr[i] == arr[k])
+			{
+				arr[i] = rand() % 20;
+			}
+		}
+		cout << arr[i] << endl;
+	}
+}
